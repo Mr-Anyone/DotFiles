05320ec0f9e7f848b0d345a02ee628011960ec4a
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2025-01-11 18:44:19 -0800

    revert back this isn't for me. Not enough contrast

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,12 +1,12 @@
--- vim.cmd[[colorscheme gruvbox]]
+vim.cmd[[colorscheme gruvbox]]
 -- vim.cmd[[colorscheme tokyonight-storm]]
 
-require("tokyonight").setup({
-    -- other configs
-    on_colors = function(colors)
-        colors.border = "#565f89"
-    end
-})
+-- require("tokyonight").setup({
+--     -- other configs
+--     on_colors = function(colors)
+--         colors.border = "#565f89"
+--     end
+-- })
 
 -- vim.g.tokyonight_colors = { border = "orange" }
-vim.cmd[[colorscheme tokyonight-night]]
+-- vim.cmd[[colorscheme tokyonight-night]]
