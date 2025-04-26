66df69a18f345adbf45ae3e1206631afc990c355
Vincent <53135664+Mr-Anyone@users.noreply.github.com>
2025-04-26 10:09:51 -0700

    added new git tool

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 rm -r ~/.config/i3
 rm -r ~/.config/nvim
 rm -r ~/.tmux.conf
+rm -r ~/.gitconfig
 
 # copy the neovim config file
 sudo ln -s $(pwd)/nvim ~/.config/nvim
 sudo ln -s $(pwd)/i3 ~/.config/i3
 sudo ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+sudo ln -s $(pwd)/.gitconfig ~/.gitconfig
+
+git config --global core.pager delta
+git config --global interactive.diffFilter 'delta --color-only'
+git config --global delta.navigate true
+git config --global merge.conflictStyle zdiff3
