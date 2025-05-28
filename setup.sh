5e7db05e25431d0de7dbd5ececb923e2d8073bd0
Vincent <llvm@viceroygroup.ca>
2025-05-28 00:23:51 -0400

    added lldb and fix some stuff

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ rm -r ~/.config/i3
 rm -r ~/.config/nvim
 rm -r ~/.config/alacritty
 rm -r ~/.tmux.conf
+rm -r ~/.lldbinit
 rm -r ~/.gitconfig
 
 # copy the neovim config file
@@ -10,8 +11,8 @@ sudo ln -s $(pwd)/i3 ~/.config/i3
 sudo ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 sudo ln -s $(pwd)/alacritty ~/.config/alacritty
 sudo ln -s $(pwd)/.gitconfig ~/.gitconfig
+sudo ln -s $(pwd)/.lldbinit ~/.lldbinit
 
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
-git config --global merge.conflictStyle zdiff3
