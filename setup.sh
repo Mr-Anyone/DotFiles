d628ae12a88318cca8c3fb9a35d52d1de8ef438d
Vincent <llvm@viceroygroup.ca>
2025-06-04 17:47:14 +0800

    added echo message

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo "Removing Directories"
 # remove directory
 rm -r ~/.config/i3
 rm -r ~/.config/nvim
@@ -6,6 +7,7 @@ rm -r ~/.tmux.conf
 rm -r ~/.lldbinit
 rm -r ~/.gitconfig
 
+echo "Creating Symbolic Link"
 # copy the neovim config file
 sudo ln -s $(pwd)/nvim ~/.config/nvim
 sudo ln -s $(pwd)/i3 ~/.config/i3
@@ -14,6 +16,7 @@ sudo ln -s $(pwd)/alacritty ~/.config/alacritty
 sudo ln -s $(pwd)/.gitconfig ~/.gitconfig
 sudo ln -s $(pwd)/.lldbinit ~/.lldbinit
 
+echo "Setting up git"
 git config --global core.pager delta
 git config --global interactive.diffFilter 'delta --color-only'
 git config --global delta.navigate true
