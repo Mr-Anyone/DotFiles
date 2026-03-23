4081c0062ac7af84bc8ef7e5a56447b911c4cf32
Vincent <llvm@viceroygroup.ca>
2026-03-23 10:07:45 -0400

    remove sudo in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,9 +10,9 @@ rm -r ~/.gitconfig
 
 echo "Creating Symbolic Link"
 # copy the neovim config file
-sudo ln -s $(pwd)/nvim ~/.config/nvim
-sudo ln -s $(pwd)/i3 ~/.config/i3
-sudo ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-sudo ln -s $(pwd)/alacritty ~/.config/alacritty
-sudo ln -s $(pwd)/.gitconfig ~/.gitconfig
-sudo ln -s $(pwd)/.lldbinit ~/.lldbinit
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/i3 ~/.config/i3
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/alacritty ~/.config/alacritty
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.lldbinit ~/.lldbinit
