b343294941d515327fabf60c70b93a4aa9a08a99
Vincent <llvm@viceroygroup.ca>
2026-07-23 12:55:09 -0400

    better default settings after rocmlir

--- nvim/lua/plugins/nerdtree.lua
+++ nvim/lua/plugins/nerdtree.lua
@@ -3,3 +3,4 @@ map("n", "<leader>nn",  "<cmd>NERDTreeFocus<cr>")
 map("n", "<leader>nf",  "<cmd>NERDTreeToggle<cr>")
 --automatically close nerdtree when a file is opened
 vim.g.NERDTreeQuitOnOpen=1
+vim.g.NERDTreeChDirMode=2
